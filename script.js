df1286c223a0e36e46b23663b16aa0ef6a4996fa
Joel Flores <j_flores88@outlook.com>
2021-07-12 14:49:59 -0700

    Creat Function to check for win

--- script.js
+++ script.js
@@ -29,8 +29,50 @@ const getScore = (board, playerSign) => {
     board.findIndex((sign, index) => {
         if(sign === playerSign) playerScore.push(index)
       })
-    // return checkforwin(playerScore)
+    compareScores(playerScore)
+};
+
+const compareScores = (score) => {
+    if(score.length < 3) return
+  
+    scoreArr = score.sort()
+    const winCondition = [
+      [0, 1, 2],
+      [0, 3, 6],
+      [0, 4, 8],
+      [1, 4, 7],
+      [2, 4, 6],
+      [2, 5, 8],
+      [3, 4, 5],
+      [6, 7, 8]
+    ]
+    winCondition.forEach(win => {
+      for(let i = 0; i < scoreArr.length; i++) {
+        if(scoreArr[i] === win[0]) {
+          checkForWin(scoreArr, win)
+        } 
+      }
+    })
   };
+
+  const checkForWin = (scores, ...wins) => {
+    let winArr = [];
+    let matchCount = 0;
+    const winner = circleTurn ? 'circle' : 'x'
+  
+    const [ winsCheck ] = wins;
+    winsCheck.forEach(win => {
+      scores.forEach(score => {
+        if(score === win) {
+          winArr.push(score)
+          matchCount++
+        }
+      })
+      if(matchCount === 3) {
+        console.log(winArr, `winner is ${winner}`) //gameovermessage
+      }
+    })
+  }
 
 cells.forEach(cell => {
     cell.addEventListener('click', cellClick, { once: true })
