0f631cb8d1d0ebaadc1c0e645334f4f1b8ea677b
Joel Flores <j_flores88@outlook.com>
2021-07-12 14:31:09 -0700

    App switches truns between x and circle

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
+const xClass = 'x';
+const circleClass = 'circle';
 const cells = document.querySelectorAll('[data-cell-index]');
-const gameBoard = new Array(9)
+const gameBoard = new Array(9);
+let circleTurn;
 
 const cellClick = (e) => {
+    const cell = e.target;
     const cellIndex = Number(e.target.attributes[1].value);
-    gameBoard[cellIndex] = 'clicked';//classlist
+    const currentClass = circleTurn ? circleClass : xClass;
+
+    markCell(cell, cellIndex, currentClass);
+    changeTurns()
+}
+
+const markCell = (cell, index, currentClass) => {
+    cell.classList.add(currentClass);
+    gameBoard[index] = currentClass;
+
     console.log(gameBoard)
+}
+
+const changeTurns = () => {
+    circleTurn = !circleTurn
 }
 
 cells.forEach(cell => {
